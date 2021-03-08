7b508a7432e18e88c1bf99be190f71647bda6746
portismouth <ozzy.gonzalez@gmail.com>
2021-03-07 19:40:04 -0600

    test

--- src/blocks/secondblock/index.js
+++ src/blocks/secondblock/index.js
@@ -7,21 +7,21 @@ registerBlockType("mytheme-blocks/secondblock", {
   description: __("Our Second Block", "mytheme-blocks"),
   category: "layout",
   icon: (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      height="24"
-      viewBox="0 0 24 24"
-      width="24"
-    >
-      <path d="M0 0h24v24H0z" fill="none" />
-      <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-    </svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+  </svg>
   ),
   keywords: [__("photo", "mytheme-blocks"), __("image", "mytheme-blocks")],
   edit: ({ className }) => {
-    return <p className={className}>Editor</p>;
+  return <p className={className}>Editor</p>;
   },
   save: () => {
-    return <p>Saved Content</p>;
+  return <p>Saved Content</p>;
   },
 });
